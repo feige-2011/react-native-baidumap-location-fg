561bb065b0da48f57d45bcf19414dfce504f86be
延飞 李 <liyanfei_2011@163.com>
2023-06-08 13:23:23 +0800

    no message

--- react-native.config.js
+++ react-native.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  dependency: {
-    platforms: {
-      ios: { project: "ios/RCTBaiduLocation.xcodeproj" },
-      android: { sourceDir: "android",
-        packageImportPath:
-            "import com.example.rctbaidulocation.BaiduLocationPackage;",
-        packageInstance: "new BaiduLocationPackage()",}
-    }
-  },
+  dependency: { platforms: { android: { sourceDir: "lib/android" } } },
   dependencies: {
     "react-native-baidumap-location-fg": {
       root: __dirname,
       platforms: {
-        ios: { podspecPath: __dirname + "/ios/RCTBaiduLocation.xcodeproj" },
-      }
-    }
-  }
+        android: {
+          sourceDir: __dirname + "/lib/android",
+          packageImportPath: "import cn.qiuxiang.react.geolocation.AMapGeolocationPackage;",
+          packageInstance: "new AMapGeolocationPackage()",
+        },
+      },
+    },
+  },
 };
